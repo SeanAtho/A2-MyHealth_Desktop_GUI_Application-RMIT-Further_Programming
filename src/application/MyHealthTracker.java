6c86bb1af32bfba2233371b2ac13e239959b371e
SeanAtho <sean@atho.com.au>
2023-04-26 12:09:49 +1000

    Updates to Current Version of MyHealthTracker

--- src/application/MyHealthTracker.java
+++ src/application/MyHealthTracker.java
@@ -1,21 +1,23 @@
 package application;
-
+import controller.HealthRecordController;
+import controller.UserController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.User;
 
 public class MyHealthTracker extends Application {
-
+    private UserController userController;
+    private HealthRecordController healthRecordController;
+    private Database database;
+    private User currentUser;
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // TODO: implement the GUI and connect it to the controllers and database
+    }
+    
     public static void main(String[] args) {
         launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-        LoginView loginView = new LoginView();
-        Scene scene = new Scene(loginView);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("My Health Tracker");
-        primaryStage.show();
     }
 }
