aa5fda7c8ce5a1d4b1d46d823d413a6f7bbd0e3b
SeanAtho <sean@atho.com.au>
2023-04-26 12:11:49 +1000

    Added Comments

--- src/application/MyHealthTracker.java
+++ src/application/MyHealthTracker.java
@@ -1,4 +1,5 @@
 package application;
+
 import controller.HealthRecordController;
 import controller.UserController;
 import javafx.application.Application;
@@ -6,17 +7,29 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import model.User;
 
+/**
+ * The main class for the MyHealthTracker application.
+ */
 public class MyHealthTracker extends Application {
-    private UserController userController;
-    private HealthRecordController healthRecordController;
-    private Database database;
-    private User currentUser;
     
+    // Attributes
+    private UserController userController;              // Controller for user-related actions
+    private HealthRecordController healthRecordController;  // Controller for health record-related actions
+    private Database database;                          // Database for storing user and health record information
+    private User currentUser;                           // The currently logged in user
+    
+    /**
+     * The entry point for the application. Starts the JavaFX GUI.
+     */
     @Override
     public void start(Stage primaryStage) throws Exception {
         // TODO: implement the GUI and connect it to the controllers and database
     }
     
+    /**
+     * The main method for the application.
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
