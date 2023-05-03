f20f909e10fa3395a34b3089112a590877fea3f0
SeanAtho <sean@atho.com.au>
2023-05-03 10:41:04 +1000

    Imports for MyHealthTrackerView

--- src/view/MyHealthTrackerView.java
+++ src/view/MyHealthTrackerView.java
@@ -1,10 +1,32 @@
 package view;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
 import controller.HealthRecordController;
 import controller.UserController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import model.HealthRecord;
 import model.User;
 
 public class MyHealthTrackerView {
